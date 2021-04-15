2e876ed1f8489dfa8dbf58316efbafec916e5403
Scott Berry <berryscottr@gmail.com>
2021-04-14 20:42:58 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # NFL Ref Sentiment Analysis
 Analyze Reddit threads for how win percentage delta affects sentiment directed towards refs
+
+@Nick Edit the paper proposal at the Google Docs link https://docs.google.com/document/d/1SghkqW9KjwRPjkTqzbE5PwmLbw2UXThVHGgy6L6pziQ/edit
+
+Gather comments from any Reddit thread by utilizing the `nfl_gamethread_scraper.py` script
+
+Place personal credentials into the `praw.Reddit` object and the thread url into the `submission` object.
+
+Uncomment the `replace_more` line to indicate how many times the script will expand the comment selection (None is ideal but has caused timeout errors for me)
+
+Modify `comment_dict_reduced` to filter out only the metadata from the reddit comments that you want. 
+I recommend taking a small sample of comments so that you can observe the structure of the comment dict and parse accordingly.
+It is contained inside a `praw.reddit.Reddit` object. A Python Console is necessary to view this easily.
+
+
+Also, Reddit rate limits us. Expect 10k comments to take about an hour. Run the script and go watch a movie or something. 
+
+`nfl_gamethread_sentiment_analysis.py` is currently basically copied from another project and has lots of editing to do don't look too much into that yet.
--- nfl_gamethread_scraper.py
+++ nfl_gamethread_scraper.py
@@ -2,31 +2,36 @@ import praw
 import numpy as np
 import json
 import codecs
-reddit = praw.Reddit(client_id='s0EbXF6ElYxSQA',
-                     client_secret='ti5S5SDRDliW2K1jujsI_SCzcAzVcg',
-                     user_agent='nflgamethreadscraper by u/scottieburr',
-                     username='user',
-                     password='pass')
-submission = reddit.submission(url='https://www.reddit.com/r/nfl/comments/kzhfxm/game_thread_tampa_bay_buccaneers_115_at_new/')
-# submission.comments.replace_more(limit=1500)
-submission_comments = np.array(submission.comments._comments)
-comments = []
-i = 0
-while i < len(submission_comments):
-    try:
-        comment = submission_comments[i]
-        comment_dict = {k: v for k, v in comment.__dict__.items() if k != '_reddit'}
-        comment_dict_reduced = {
-            'timestamp': comment_dict['created_utc'],
-            'body': comment_dict['body'],
-            'flair': comment_dict['author_flair_css_class']
-        }
-        comment_array = np.asarray(comment_dict_reduced)
-        comment_list = comment_array.tolist()
-        comments.append(comment_list)
-    except:
-        pass
-    i += 1
-submission_comments_array = np.asarray(comments)
-submission_comments_list = submission_comments_array.tolist()
-json.dump(submission_comments_list, codecs.open('nfl_gamethread_comments.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+
+if __name__ == "__main__":
+    reddit = praw.Reddit(client_id='s0EbXF6ElYxSQA',
+                         client_secret='ti5S5SDRDliW2K1jujsI_SCzcAzVcg',
+                         user_agent='nflgamethreadscraper by u/scottieburr',
+                         # Insert your values here
+                         username='user',
+                         password='pass')
+    # Insert your thread url here
+    submission = reddit.submission(url='https://www.reddit.com/r/nfl/comments/kzhfxm/game_thread_tampa_bay_buccaneers_115_at_new/')
+    # Uncomment and set number of times the script will load more comments
+    # submission.comments.replace_more(limit=1500)
+    submission_comments = np.array(submission.comments._comments)
+    comments = []
+    i = 0
+    while i < len(submission_comments):
+        try:
+            comment = submission_comments[i]
+            comment_dict = {k: v for k, v in comment.__dict__.items() if k != '_reddit'}
+            comment_dict_reduced = {
+                'timestamp': comment_dict['created_utc'],
+                'body': comment_dict['body'],
+                'flair': comment_dict['author_flair_css_class']
+            }
+            comment_array = np.asarray(comment_dict_reduced)
+            comment_list = comment_array.tolist()
+            comments.append(comment_list)
+        except:
+            pass
+        i += 1
+    submission_comments_array = np.asarray(comments)
+    submission_comments_list = submission_comments_array.tolist()
+    json.dump(submission_comments_list, codecs.open('nfl_gamethread_comments.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
